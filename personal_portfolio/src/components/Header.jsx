95a81e35d6b9c84c7696ddf5f18864f9aaceb14a
ahanooman0307 <ahanooman0307@gmail.com>
2023-07-26 17:07:46 -0400

    added header

--- personal_portfolio/src/components/Header.jsx
+++ personal_portfolio/src/components/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import '../styles/Header.css'
 
 const Header = () =>{
-    <div className="container">
-        
+    return( 
+    <div className="header">
+        <div className="left">Akshay Hanooman</div>
+        <div className="right">
+            <button className="about">About</button>
+            <button className="skills">Skills</button>
+            <button className="projects">Project</button>
+            <button className="contact">Contact</button>
+        </div>
     </div>
+    )
 }
+
+export default Header;
--- personal_portfolio/src/components/Main.jsx
+++ personal_portfolio/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import Introduction from './Introduction';
+import Header from './Header';
 
 const Main = () =>{
     return( 
-
-    
+<div>
+    <Header></Header>
     <Introduction></Introduction>
+    </div>
 
     )
 };
